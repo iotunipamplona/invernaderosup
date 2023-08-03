ad483314173b27ae6f7e5d36d5693bfd466dff15
iotunipamplona <iotunipamplona@gmail.com>
2023-08-03 12:31:45 -0500

    Update mongo.js

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
+const uri = process.env.MONGODB_URI; // Asegúrate de que esta variable de entorno esté configurada
+
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MONGODB Atlas");
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected successfully");
   } catch (error) {
-    console.error(error);
+    console.error("Error connecting to database:", error);
   }
 };
 
