f06857e3df6984349cd101e1020b86ba66e48a0b
iotunipamplona <iotunipamplona@gmail.com>
2023-08-03 15:35:19 -0500

    tarde

--- backend/routes/co2i1.js
+++ backend/routes/co2i1.js
@@ -1,25 +1,24 @@
 const express =require("express");
 const { getco2i1s, getco2i1, createco2i1, updateco2i1, deleteco2i1 } = require("../controllers/co2i1");
 const router = express.Router();
-const {validatorCreateco2i1, validatorGetco2i1} = require("../validators/co2i1")
 /**Lista los usuarios */
 
 router.get("/",getco2i1s);
 
 /**Obtener un detalle de usuario */
 
-router.get("/:id", validatorGetco2i1, getco2i1);
+router.get("/:id", getco2i1);
 
 /**Crea un registro de usuario */
-router.post("/", validatorCreateco2i1, createco2i1);
+router.post("/", createco2i1);
 
 
 /**Actualizar un registro de usuario */
-router.put("/:id", validatorGetco2i1, validatorCreateco2i1, updateco2i1);
+router.put("/:id", updateco2i1);
 
 /**Eliminar un rusuario */
 
-router.delete("/:id", validatorGetco2i1, deleteco2i1);
+router.delete("/:id", deleteco2i1);
 
 
 
--- backend/routes/co2i2.js
+++ backend/routes/co2i2.js
@@ -1,81 +1,25 @@
-const mongoose = require("mongoose");
-const {co2i2Model} = require('../models');
-const { handleHttpError } = require('../utils/handleError');
-const { matchedData } = require("express-validator");
-/** 
- * Obtener lista de la base de datos
- * @param {*} req
- * @param {*} res
- */
-const getco2i2s = async (req, res) =>{
-  try{
-  const data = await co2i2Model.find({})
-  res.send({data});
-  }catch(e){
-    handleHttpError(res,'ERROR EN GET USUARIOS');
-  }
-};
-/** 
- * Obtener un detalle
- * @param {*} req
- * @param {*} res
- */
-const getco2i2 = async (req, res) =>{
-   try{
-    req =matchedData(req);
-    const {id} = req;
-    const data = await co2i2Model.findById(id);
-    res.send({data});
-   }catch(e){
-    handleHttpError(res,"ERROR EN GET USUARIO");
-  }
- 
-};
-/** 
- * Insertar un registro
- * @param {*} req
- * @param {*} res
- */
-const createco2i2 = async(req,res) =>{
-  try{
-    const body = matchedData(req)
-    const data = await co2i2Model.create(body);
-    res.send({data});
-     }catch(e){
-       handleHttpError(res,"ERROR CREANDO USUARIOS");
-     }
-};
-/** 
- * Actualizar un registro
- * @param {*} req
- * @param {*} res
- */
-const updateco2i2 = async (req, res) => {
-  try {
-    const {id, ...body} = matchedData(req);
-    const data = await co2i2Model.findOneAndUpdate(id, body 
-    );
-    res.send({ data });
-  } catch (e) {
-    handleHttpError(res, e);
-  }
-};
-/** 
- * Eliminar un registro
- * @param {*} req
- * @param {*} res
- */
-const deleteco2i2 = async (req,res) =>{  
-    try{
+const express =require("express");
+const { getco2i2s, getco2i2, createco2i2, updateco2i2, deleteco2i2 } = require("../controllers/co2i2");
+const router = express.Router();
+/**Lista los usuarios */
 
-  req = matchedData(req);
-  const {id} = req;
-  const data = await co2i2Model.deleteOne({_id:id});
-  res.send({data});
- }catch(e){
-  handleHttpError(res,"ERROR");
-}
-};
+router.get("/",getco2i2s);
+
+/**Obtener un detalle de usuario */
+
+router.get("/:id", getco2i2);
+
+/**Crea un registro de usuario */
+router.post("/", createco2i2);
 
 
-module.exports={getco2i2s, getco2i2, createco2i2, updateco2i2, deleteco2i2};
+/**Actualizar un registro de usuario */
+router.put("/:id", updateco2i2);
+
+/**Eliminar un rusuario */
+
+router.delete("/:id", deleteco2i2);
+
+
+
+module.exports = router;
